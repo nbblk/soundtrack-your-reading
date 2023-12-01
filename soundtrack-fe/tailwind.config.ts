3458f21464877a7c9aed458ed7e36bd209e3befe
nbblk <nbblks@gmail.com>
2023-12-01 22:52:54 +0900

    style: add loader component

--- soundtrack-fe/tailwind.config.ts
+++ soundtrack-fe/tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        flash: {
+          "0%": {
+            "background-color": "#FFF2",
+            "box-shadow": "32px 0 #FFF2, -32px 0 #FFF",
+          },
+          "50%": {
+            "background-color": "#FFF",
+            "box-shadow": "32px 0 #FFF2, -32px 0 #FFF2",
+          },
+          "100%": {
+            "background-color": "FFF2",
+            "box-shadow": "32px 0 #FFF, -32px 0 #FFF2",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
