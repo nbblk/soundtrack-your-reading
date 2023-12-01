9dea5f34dcb4c8102b666f34371adcec005e2735
nbblk <nbblks@gmail.com>
2023-12-01 22:53:26 +0900

    style: add footer component

---
+++ soundtrack-fe/app/components/shared/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-full h-[150px] border border-black bg-black">
+      <h1 className="text-white">Footer</h1>
+    </footer>
+  );
+}
