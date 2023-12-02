ca96cc7e01b1d202ff2302318ed38ef02e57f54f
nbblk <nbblks@gmail.com>
2023-12-03 00:38:16 +0900

    modifiy: replace logo and fix sticky nav

--- soundtrack-fe/app/components/shared/Navbar.tsx
+++ soundtrack-fe/app/components/shared/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Searchbar from "./Searchbar";
@@ -9,7 +9,7 @@ export default function Navbar() {
   const { login, signup } = useAuth();
 
   return (
-    <nav className="sticky w-full h-auto md:h-[80px] m-0 p-4 border border-t-0 border-x-0 border-black">
+    <nav className="sticky top-0 w-full h-auto md:h-[80px] m-0 p-4 border border-t-0 border-x-0 border-black bg-white">
       <ul className="w-full h-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-8">
         <li className="w-full md:w-1/3 flex flex-col md:flex-row items-center gap-4">
           <Link
@@ -17,19 +17,19 @@ export default function Navbar() {
             className="h-auto p-4 flex justify-start items-center "
           >
             <Image
-              src="/images/logo.svg"
+              src="/images/logo.jpg"
               alt="soundtrack-your-reading"
-              width="0"
-              height="0"
-              className="aspect-ratio max-w-[100px] max-w-[100px] w-auto h-auto"
+              width="100"
+              height="100"
+              className="max-w-[100px] max-h-[100px]"
               priority={true}
             />
           </Link>
           <Searchbar />
         </li>
         <ul className="md:w-1/5 w-full flex flex-col md:flex-row justify-center md:justify-end items-center md:items-end gap-4">
-            <Button type="button" size="md" value="Login" onClick={login} />            
-            <Button type="button" size="md" value="Signup" onClick={signup} />            
+          <Button type="button" size="md" value="Login" onClick={login} />
+          <Button type="button" size="md" value="Signup" onClick={signup} />
         </ul>
       </ul>
     </nav>
