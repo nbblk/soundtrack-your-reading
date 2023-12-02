7333918057c5127384ec4e80ad13a4bb2ce3e6a9
nbblk <nbblks@gmail.com>
2023-12-02 20:39:22 +0900

    refactor: add component index file

---
+++ soundtrack-fe/app/components/shared/index.ts
@@ -0,0 +1,23 @@
+import Button from "./Button";
+import Card from "./Card";
+import Input from "./Input";
+import Modal from "./Modal";
+import Footer from "./Footer";
+import Form from "./Form";
+import Dropdown from "./Dropdown";
+import Loader from "./Loader";
+import Searchbar from "./Searchbar";
+import UserProfile from "./UserProfile";
+
+export {
+  Button,
+  Card,
+  Footer,
+  Form,
+  Input,
+  Modal,
+  Dropdown,
+  Loader,
+  Searchbar,
+  UserProfile,
+};
