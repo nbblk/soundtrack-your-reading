ffd5694956e3bcf7d8e7fda3398ddf4e5085dd99
nbblk <nbblks@gmail.com>
2023-12-02 20:54:35 +0900

    feat: add google auth provider on page

--- soundtrack-fe/app/page.tsx
+++ soundtrack-fe/app/page.tsx
@@ -1,14 +1,17 @@
 import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex flex-col items-center justify-between">
-      <Navbar />
-      <section className="w-full">
-      this is the main page
-      </section>
-      <Footer />
-    </main>
+    <GoogleOAuthProvider
+      clientId={process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID || ""}
+    >
+      <main className="w-full h-full flex flex-col items-center justify-between">
+        <Navbar />
+        <section className="w-full">this is the main page</section>
+        <Footer />
+      </main>
+    </GoogleOAuthProvider>
   );
 }
