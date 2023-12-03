235203c422c03fb6c215ec6ff2cc14d0b17bdd77
nbblk <nbblks@gmail.com>
2023-12-03 12:37:40 +0900

    feat: add /mypage/posts page with dummies

--- soundtrack-fe/app/mypage/posts/page.tsx
+++ soundtrack-fe/app/mypage/posts/page.tsx
@@ -1,7 +1,27 @@
-const Posts = () => {   
-    return (
-        <div>
-            <h1>Posts</h1>
-        </div>
-    )
-};
+import { Post } from "@/app/lib/types/Post";
+import { dummyPosts } from "./dummy";
+import Card from "@/app/components/shared/Card";
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <section className="w-full h-auto p-8 flex justify-start items-center">
+      <ul className="w-full flex flex-wrap justify-center items-center gap-8">
+        {dummyPosts.map((post: Post) => (
+          <Card key={post.id} size="md" title={post.title}>
+            <p className="w-full text-ellipsis overflow-hidden">
+              {post.content}
+            </p>
+            <div className="w-full flex flex-col justify-end items-end">
+              <small>created at {post.createdAt}</small>
+              <small>updated at {post.updatedAt}</small>
+            </div>
+          </Card>
+        ))}
+      </ul>
+    </section>
+  );
+}
