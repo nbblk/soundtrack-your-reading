0986859972d96d32b05700812e5ea03dfeda9678
nbblk <nbblks@gmail.com>
2023-12-03 16:27:38 +0900

    style: adjust navbar z-index

--- soundtrack-fe/app/components/shared/Navbar.tsx
+++ soundtrack-fe/app/components/shared/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const { login, signup } = useAuth();
 
   return (
-    <nav className="sticky top-0 w-full h-auto md:h-[80px] m-0 p-4 border border-t-0 border-x-0 border-black bg-white">
+    <nav className="sticky top-0 w-full h-auto md:h-[80px] m-0 p-4 z-50 border border-t-0 border-x-0 border-black bg-white">
       <ul className="w-full h-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-8">
         <li className="w-full md:w-1/3 flex flex-col md:flex-row items-center gap-4">
           <Link
