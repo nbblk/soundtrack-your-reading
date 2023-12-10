1dacea8a9d0fdf82b5646c02a01f28f742b4dcfb
nbblk <nbblks@gmail.com>
2023-12-10 23:41:53 +0900

    modify: rewrite routes in ts

--- soundtrack-be/router.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router");
-const router = new Router();
-const { getEvents, postEvent } = require("./controllers/events.controllers");
-
-router.get("/events_list", getEvents);
-router.post("/add_event", postEvent);
-
-module.exports = router;
